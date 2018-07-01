734d91a82a078074021b99e6a497cd1780f19d49
FilipSavic95 <filipsavic1995@yahoo.com>
2018-07-02 01:40:02 +0200

    Fixed PCA call.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.model_selection import train_test_split
-
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
 
 def read_file(file):
     """
@@ -15,6 +18,7 @@ def read_file(file):
     data = pd.read_csv(file)
     print(data.head())
     x = data[feature_columns]
+    # x = data[ [*feature_columns, 'quality'] ]
     y = data[["quality"]]
     print(y)
     return x, y
@@ -42,22 +46,48 @@ def create_train_test_files(dataset_file, train_path, test_path):
 def test_model(train_file):
     x, y = read_file(train_file)
 
-    sss = StratifiedShuffleSplit(n_splits=5, test_size=0.25, random_state=1)
-    for train_index, test_index in sss.split(x, y):
-        X_train, X_test = x.loc[train_index], x.loc[test_index]
-        y_train, y_test = y.loc[train_index], y.loc[test_index]
-        # print(y_train.value_counts(sort=False, normalize=True))
-        print(test_index)
-        # print(y_test.value_counts(sort=False, normalize=True))
-        print()
+    # First center and scale the data
+    scaled_data = preprocessing.scale(x) # scale funkcija ocekuje da torke budu u redovima, ne kolonama
+
+    pca = PCA()  # create a PCA object
+    pca.fit(scaled_data)  # do the math
+    pca_data = pca.transform(scaled_data)  # get PCA coordinates for scaled_data
+    print(pca_data)
+
+    # The following code constructs the Scree plot
+    per_var = np.round(pca.explained_variance_ratio_ * 100, decimals=1)
+    labels = ['PC' + str(x) for x in range(1, len(per_var) + 1)]
+
+    plt.bar(x=range(1, len(per_var) + 1), height=per_var, tick_label=labels)
+    plt.ylabel('Percentage of Explained Variance')
+    plt.xlabel('Principal Component')
+    plt.title('Scree Plot')
+    plt.show()
+
+    # Determine which vine properties had the biggest influence on PC1
+    ## get the name of the top 10 measurements (vine properties) that contribute
+    ## most to pc1.
+    ## first, get the loading scores
+    loading_scores = pd.Series(pca.components_[0], index=['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
+                       'free sulfur dioxide',
+                       'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol'])
+    ## now sort the loading scores based on their magnitude
+    sorted_loading_scores = loading_scores.abs().sort_values(ascending=False)
+
+    # get the names of the top 10 vine properties
+    top_10_properties = sorted_loading_scores[0:10].index.values
+
+    ## print the gene names and their scores (and +/- sign)
+    print(loading_scores[top_10_properties])
+    # sa > https://statquest.org/2018/01/08/statquest-pca-in-python/
 
 
 def main():
     train_path = '../data/train.csv'
     test_path = '../data/test.csv'
     # main(train_path, test_path)
-    # test_model(train_path)
-    create_train_test_files("../data/winequality-red.csv", train_path, test_path)
+    test_model(train_path)
+    # create_train_test_files("../data/winequality-red.csv", train_path, test_path)
 
 
 if __name__ == '__main__':
